bd89808ac2e827e2b433d2d4e9b79e810fc3d942
gnosticka <gnosticka@gmail.com>
2025-12-17 20:15:58 +1100

    init

---
+++ src/main/java/app/gnostex/gnostexbackend/GnostexBackendApplication.java
@@ -0,0 +1,13 @@
+package app.gnostex.gnostexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnostexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnostexBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/gnostex/gnostexbackend/GnostexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.gnostex.gnostexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnostexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
